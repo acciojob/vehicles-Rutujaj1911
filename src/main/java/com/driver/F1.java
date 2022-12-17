be3ae876acaa24684d0a4a80cfcd18e410bf9e24
Rutuja <rutujajadhav896@gmail.com>
2022-12-17 20:43:10 +0530

    Message

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -26,13 +26,13 @@ public class F1 extends Car {
             changeGear(1);
         }else if(newSpeed>1 && newSpeed<=50){
             changeGear(1);
-        }else if(newSpeed>51 && newSpeed<=100){
+        }else if(newSpeed>=51 && newSpeed<=100){
             changeGear(2);
-        }else if(newSpeed>101 && newSpeed<=150){
+        }else if(newSpeed>=101 && newSpeed<=150){
             changeGear(3);
-        }else if(newSpeed>151 && newSpeed<=200){
+        }else if(newSpeed>=151 && newSpeed<=200){
             changeGear(4);
-        }else if(newSpeed>201 && newSpeed<=250){
+        }else if(newSpeed>=201 && newSpeed<=250){
             changeGear(5);
         }else{
             changeGear(6);
